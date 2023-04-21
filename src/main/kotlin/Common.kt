7b1364a2e843dff6fc94848b9f3b7492ef75c99f
Lev Kolomazov <levkachz@gmail.com>
2023-04-21 14:42:25 +0200

    test1

--- src/main/kotlin/Common.kt
+++ src/main/kotlin/Common.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedReader
 
 abstract class Common(var inputFileName: String, isTest: Boolean) {
-    var input: BufferedReader
+    var input: BufferedReader;
 
     init {
         inputFileName = "$inputFileName/$inputFileName" // files are in resources/{input}/
